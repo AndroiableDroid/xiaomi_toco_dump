e00a2341b8d5676e4171c2e8cd04db19a847dd37
AndroiableDroid <mohd.faraz.abc@gmail.com>
2020-12-31 14:24:34 +0530

    Add extras for toco-user 10 QKQ1.190825.002 V12.0.3.0.QFNMIXM release-keys
    Signed-off-by: AndroiableDroid <mohd.faraz.abc@gmail.com>

---
+++ README.md
@@ -0,0 +1,16 @@
+## toco-user 10 QKQ1.190825.002 V12.0.3.0.QFNMIXM release-keys
+- Manufacturer: xiaomi
+- Platform: sm6150
+- Codename: toco
+- Brand: Xiaomi
+- Flavor: toco-user
+- Release: 10
+- Id: QKQ1.190825.002
+- Incremental: V12.0.3.0.QFNMIXM
+- Tags: release-keys
+- Fingerprint: Xiaomi/toco_global/toco:10/QKQ1.190825.002/V12.0.3.0.QFNMIXM:user/release-keys
+- Branch: toco-user-10-QKQ1.190825.002-V12.0.3.0.QFNMIXM-release-keys
+- Repo: xiaomi_toco_dump
+
+
+>Dumped by [Phoenix Firmware Dumper](https://github.com/DroidDumps/phoenix_firmware_dumper)
---
+++ join_split_files.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cat bootimg/13_dtbdump_Qualcomm_Technologies,_Inc._SM6150P_SoC.dtb.* 2>/dev/null >> bootimg/13_dtbdump_Qualcomm_Technologies,_Inc._SM6150P_SoC.dtb
+rm -f bootimg/13_dtbdump_Qualcomm_Technologies,_Inc._SM6150P_SoC.dtb.* 2>/dev/null
+cat system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> system/system/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f system/system/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null >> system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk
+rm -f system/system/app/MiuiVideoPlayer/MiuiVideoPlayer.apk.* 2>/dev/null
+cat system/system/apex/com.android.runtime.release.apex.* 2>/dev/null >> system/system/apex/com.android.runtime.release.apex
+rm -f system/system/apex/com.android.runtime.release.apex.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/Settings/Settings.apk.* 2>/dev/null >> product/priv-app/Settings/Settings.apk
+rm -f product/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null >> product/app/WebViewGoogle/WebViewGoogle.apk
+rm -f product/app/WebViewGoogle/WebViewGoogle.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.opera.browser_38/partner-com.opera.browser_38.apk.* 2>/dev/null >> cust/app/customized/partner-com.opera.browser_38/partner-com.opera.browser_38.apk
+rm -f cust/app/customized/partner-com.opera.browser_38/partner-com.opera.browser_38.apk.* 2>/dev/null
+cat cust/app/customized/partner-ctrip.english_34/partner-ctrip.english_34.apk.* 2>/dev/null >> cust/app/customized/partner-ctrip.english_34/partner-ctrip.english_34.apk
+rm -f cust/app/customized/partner-ctrip.english_34/partner-ctrip.english_34.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.lazada.android_86/partner-com.lazada.android_86.apk.* 2>/dev/null >> cust/app/customized/partner-com.lazada.android_86/partner-com.lazada.android_86.apk
+rm -f cust/app/customized/partner-com.lazada.android_86/partner-com.lazada.android_86.apk.* 2>/dev/null
+cat cust/app/customized/partner-com.alibaba.aliexpresshd_54/partner-com.alibaba.aliexpresshd_54.apk.* 2>/dev/null >> cust/app/customized/partner-com.alibaba.aliexpresshd_54/partner-com.alibaba.aliexpresshd_54.apk
+rm -f cust/app/customized/partner-com.alibaba.aliexpresshd_54/partner-com.alibaba.aliexpresshd_54.apk.* 2>/dev/null
+cat recovery.img.* 2>/dev/null >> recovery.img
+rm -f recovery.img.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
